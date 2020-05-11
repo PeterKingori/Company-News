a7442315d4c24636569c555168f59af9119bac28
Peter Kingori <kpndegwa@gmail.com>
2020-05-11 05:56:13 +0300

    Minor changes to remove unnecessary methods

--- src/main/java/dao/NewsDao.java
+++ src/main/java/dao/NewsDao.java
@@ -7,7 +7,6 @@ import java.util.List;
 public interface NewsDao {
     //create
     void add(News news);
-//    void addNewsToDepartment(News news, Department department);
 
     //read
     List<News> getAllNews();
--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -7,7 +7,6 @@ import java.util.List;
 public interface UserDao {
     //create
     void add(User user);
-//    void addUserToDepartment(User user, Department department);
 
     //read
     List<User> getAllUsers();
